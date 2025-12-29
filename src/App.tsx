f6c7c269eb59d6c6ead4cf63644390112178a8f7
RimZotik <radiantzeal@mail.ru>
2025-12-29 21:37:23 +0300

    feat(window): add custom close and minimize buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import PageContainer from "./components/PageContainer/PageContainer";
+import WindowControls from "./components/WindowControls/WindowControls";
 
 function App() {
   return (
     <div className="flex w-screen h-screen overflow-hidden">
       <Sidebar />
       <PageContainer />
+      <WindowControls />
     </div>
   );
 }
