a0c8c95902222df48f105f9ae40f46b24f42f8f1
RimZotik <radiantzeal@mail.ru>
2025-12-30 00:20:09 +0300

    feat(sidebar): add app logo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,13 +5,24 @@ function Sidebar() {
 
   return (
     <aside
-      className="w-[70px] h-screen bg-[#10262F] flex flex-col"
+      className="w-[70px] h-screen bg-[#10262F] flex flex-col items-center"
       style={{
         boxShadow: "10px 0 8.5px 0 #10262F",
       }}
       data-tauri-drag-region
       onContextMenu={handleContextMenu}
-    ></aside>
+    >
+      <img
+        src="/assets/deadlyze_logo.png"
+        alt="DeadLyze"
+        className="mt-2"
+        style={{
+          width: "54px",
+          height: "54px",
+          pointerEvents: "none",
+        }}
+      />
+    </aside>
   );
 }
 
